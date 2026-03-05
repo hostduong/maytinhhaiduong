09a151714f383ffcd8389511c2382dc88197dee8
hostduong <hostduong@gmail.com>
2026-03-05 21:00:56 +0700

    Update m_saas_limit.go

--- middlewares/m_saas_limit.go
+++ middlewares/m_saas_limit.go
@@ -1,21 +1,33 @@
 package middlewares
 
 import (
+	"net/http"
+	"app/core"
+
 	"github.com/gin-gonic/gin"
 )
 
 // CheckSaaSLimit: Trạm kiểm soát tài nguyên gói cước
 func CheckSaaSLimit(resourceType string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// shopID := c.GetString("SHOP_ID")
+		shopID := c.GetString("SHOP_ID")
 		
-		// TODO: Tương lai sẽ móc vào bảng cấu hình gói cước của Master để check.
-		// Ví dụ: KiemTraGioiHan(shopID, "so_luong_san_pham")
-		// Nếu vượt quá:
-		// TuChoiTruyCap(c, http.StatusPaymentRequired, "Gói dịch vụ của bạn đã hết dung lượng. Vui lòng nâng cấp!")
-		// return
+		if resourceType == "san_pham" {
+			maxSP := core.LayGioiHanSanPhamCuaShop(shopID)
+			if maxSP != -1 {
+				// Cần khóa RLock để đếm số lượng hiện tại một cách an toàn
+				lock := core.GetSheetLock(shopID, core.TenSheetMayTinh)
+				lock.RLock()
+				currentCount := len(core.CacheSanPhamMayTinh[shopID])
+				lock.RUnlock()
+
+				if currentCount >= maxSP {
+					TuChoiTruyCap(c, http.StatusPaymentRequired, "Gói dịch vụ của bạn đã đạt giới hạn tối đa. Vui lòng nâng cấp gói cước!")
+					return
+				}
+			}
+		}
 		
-		// Hiện tại pass qua cửa này
 		c.Next()
 	}
 }
