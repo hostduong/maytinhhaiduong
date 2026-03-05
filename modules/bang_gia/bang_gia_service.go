921f332ad47de5a4970c0735b82ffcead522d442
hostduong <hostduong@gmail.com>
2026-03-05 16:17:44 +0700

    Update bang_gia_service.go

--- modules/bang_gia/bang_gia_service.go
+++ modules/bang_gia/bang_gia_service.go
@@ -1,49 +1,4 @@
-package bang_gia
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net/http"
-	"time"
-
-	"app/core"
-	"app/modules/thanh_toan"
-)
-
-type BangGiaService struct {
-	paySvc thanh_toan.PaymentService
-}
-
-// KhoiTaoHaTangSubdomain: Chạy ngầm gọi API Google Cloud Run (Goroutine)
-func (s *BangGiaService) KhoiTaoHaTangSubdomain(tenDangNhap string) {
-	subdomain := fmt.Sprintf("%s.99k.vn", tenDangNhap)
-	
-	payload := map[string]interface{}{
-		"apiVersion": "domains.cloudrun.com/v1",
-		"kind":       "DomainMapping",
-		"metadata": map[string]string{"name": subdomain},
-		"spec":     map[string]interface{}{"routeName": "maytinhhaiduong"},
-	}
-	
-	body, _ := json.Marshal(payload)
-	apiURL := "https://asia-southeast1-run.googleapis.com/apis/domains.cloudrun.com/v1/namespaces/project-47337221-fda1-48c7-b2f/domainmappings"
-	
-	req, _ := http.NewRequest("POST", apiURL, bytes.NewBuffer(body))
-	// [QUAN TRỌNG]: Token xác thực cần được truyền ở đây
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{Timeout: 30 * time.Second}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("[HẠ TẦNG] Lỗi kết nối API: %v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-	
-	fmt.Printf("[HẠ TẦNG] Kích hoạt Subdomain %s (Status: %d)\n", subdomain, resp.StatusCode)
-}
 
 // BuyStarterPackage: Thực thi nghiệp vụ mua gói và bẻ lái về Master
 func (s *BangGiaService) BuyStarterPackage(masterShopID, userID, maGoi, maCode string) (string, error) {
