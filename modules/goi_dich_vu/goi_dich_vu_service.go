13547b39eaf5cfbd194dc5a8916691906b363ad9
hostduong <hostduong@gmail.com>
2026-03-06 16:17:33 +0700

    Update goi_dich_vu_service.go

--- modules/goi_dich_vu/goi_dich_vu_service.go
+++ modules/goi_dich_vu/goi_dich_vu_service.go
@@ -6,15 +6,13 @@ import (
 	"errors"
 )
 
-type Service struct { repo Repo }
-
 type DTO_LuuGoiDichVu struct {
 	IsNew          bool
 	MaGoi          string
 	TenGoi         string
 	LoaiGoi        string
 	ThoiHanNgay    int
-	ThoiHanHienThi string // MỚI
+	ThoiHanHienThi string 
 	GiaNiemYet     float64
 	GiaBan         float64
 	CodesJson      string 
@@ -27,37 +25,37 @@ type DTO_LuuGoiDichVu struct {
 	TrangThai      int
 }
 
-func (s *Service) XuLyLuu(shopID string, input DTO_LuuGoiDichVu) error {
+func Service_XuLyLuu(shopID string, input DTO_LuuGoiDichVu) error {
 	if input.MaGoi == "" || input.TenGoi == "" { return errors.New("Mã và Tên gói là bắt buộc") }
 
 	if input.IsNew {
-		if _, exist := s.repo.FindByCode(shopID, input.MaGoi); exist { return errors.New("Mã gói đã tồn tại") }
+		if _, exist := Repo_FindByCode(shopID, input.MaGoi); exist { return errors.New("Mã gói đã tồn tại") }
 		
 		newG := &core.GoiDichVu{
 			MaGoi: input.MaGoi, TenGoi: input.TenGoi, LoaiGoi: input.LoaiGoi,
-			ThoiHanNgay: input.ThoiHanNgay, ThoiHanHienThi: input.ThoiHanHienThi, // MỚI
+			ThoiHanNgay: input.ThoiHanNgay, ThoiHanHienThi: input.ThoiHanHienThi,
 			GiaNiemYet: input.GiaNiemYet, GiaBan: input.GiaBan,
 			MaCodeKichHoatJson: input.CodesJson, GioiHanJson: input.GioiHanJson,
 			MoTa: input.MoTa, NhanHienThi: input.NhanHienThi, NgayBatDau: input.NgayBatDau,
 			NgayKetThuc: input.NgayKetThuc, SoLuongConLai: input.SoLuongConLai, TrangThai: input.TrangThai,
 		}
 		_ = json.Unmarshal([]byte(input.CodesJson), &newG.DanhSachCode)
-		s.repo.Insert(shopID, newG)
+		Repo_Insert(shopID, newG)
 	} else {
-		g, ok := s.repo.FindByCode(shopID, input.MaGoi)
+		g, ok := Repo_FindByCode(shopID, input.MaGoi)
 		if !ok { return errors.New("Không tìm thấy gói để cập nhật") }
 
-		lock := core.GetSheetLock(shopID, core.TenSheetGoiDichVu)
+		lock := core.GetSheetLock(shopID, core.TenSheetGoiDichVuMaster)
 		lock.Lock()
 		g.TenGoi = input.TenGoi; g.LoaiGoi = input.LoaiGoi; g.ThoiHanNgay = input.ThoiHanNgay
-		g.ThoiHanHienThi = input.ThoiHanHienThi // MỚI
+		g.ThoiHanHienThi = input.ThoiHanHienThi 
 		g.GiaNiemYet = input.GiaNiemYet; g.GiaBan = input.GiaBan; g.MaCodeKichHoatJson = input.CodesJson
 		g.GioiHanJson = input.GioiHanJson; g.MoTa = input.MoTa; g.NhanHienThi = input.NhanHienThi
 		g.SoLuongConLai = input.SoLuongConLai; g.TrangThai = input.TrangThai
 		_ = json.Unmarshal([]byte(input.CodesJson), &g.DanhSachCode)
 		lock.Unlock()
 
-		s.repo.Update(shopID, g)
+		Repo_Update(shopID, g)
 	}
 	return nil
 }
