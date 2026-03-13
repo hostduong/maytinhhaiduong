803dc0aa59c0271d38e12f252f139c7e00c6ef9b
hostduong <hostduong@gmail.com>
2026-03-13 07:12:07 +0700

    Update thanh_vien_master_repo.go

--- modules/thanh_vien_master/thanh_vien_master_repo.go
+++ modules/thanh_vien_master/thanh_vien_master_repo.go
@@ -2,21 +2,29 @@ package thanh_vien_master
 
 import "app/core"
 
-func Repo_LayKhachHang(shopID, userID string) (*core.KhachHang, bool) {
-	return core.LayKhachHang(shopID, userID)
+// [FIX] Tự viết hàm Lấy Khách Hàng chuyên biệt cho Master để Lock đúng Sheet
+func Repo_LayKhachHangMaster(masterID, userID string) (*core.KhachHang, bool) {
+	lock := core.GetSheetLock(masterID, core.TenSheetKhachHangMaster)
+	lock.RLock()
+	defer lock.RUnlock()
+	kh, ok := core.CacheMapKhachHang[core.TaoCompositeKey(masterID, userID)]
+	return kh, ok
 }
 
-func Repo_LayDanhSach(shopID string) []*core.KhachHang {
-	return core.LayDanhSachKhachHang(shopID)
+func Repo_LayDanhSachMaster(masterID string) []*core.KhachHang {
+	lock := core.GetSheetLock(masterID, core.TenSheetKhachHangMaster)
+	lock.RLock()
+	defer lock.RUnlock()
+	return core.CacheKhachHang[masterID]
 }
 
 func Repo_LayCapBac(shopID, userID, role string) int {
 	return core.LayCapBacVaiTro(shopID, userID, role)
 }
 
-// Bắn thẳng 1 lệnh JSON xuống Queue (Tạm biệt 26 lệnh update cũ)
 func Repo_GhiCapNhatJSONXuongQueue(shopID string, dong int, jsonStr string) {
-	core.ThemVaoHangCho(shopID, core.TenSheetKhachHang, dong, core.CotKH_DataJSON, jsonStr)
+	// [FIX LỚN] Bắn đúng vào KHACH_HANG_MASTER thay vì KHACH_HANG
+	core.ThemVaoHangCho(shopID, core.TenSheetKhachHangMaster, dong, core.CotKH_DataJSON, jsonStr)
 }
 
 func Repo_ThemTinNhanMoi(shopID string, tn *core.TinNhan) {
