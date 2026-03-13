a344b16be8a6b82c08c6d974b4824f33a1cc55a3
hostduong <hostduong@gmail.com>
2026-03-13 22:26:26 +0700

    Update auth_admin_view.go

--- auth/auth_admin/auth_admin_view.go
+++ auth/auth_admin/auth_admin_view.go
@@ -25,13 +25,13 @@ func checkLogin(c *gin.Context) bool {
 }
 
 func TrangDangNhap(c *gin.Context) {
-	if checkLogin(c) { c.Redirect(http.StatusFound, "https://admin.99k.vn/tong-quan"); return }
+	if checkLogin(c) { c.Redirect(http.StatusFound, "https://shop.99k.vn/tong-quan"); return }
 	// Render file /themes/template_admin/dang_nhap_admin.html
 	c.HTML(http.StatusOK, "dang_nhap_admin", gin.H{"TieuDe": "Đăng Nhập Quản Trị Hệ Thống"})
 }
 
 func TrangDangKy(c *gin.Context) {
-	if checkLogin(c) { c.Redirect(http.StatusFound, "https://admin.99k.vn/tong-quan"); return }
+	if checkLogin(c) { c.Redirect(http.StatusFound, "https://shop.99k.vn/tong-quan"); return }
 	// Render file /themes/template_admin/dang_ky_admin.html
 	c.HTML(http.StatusOK, "dang_ky_admin", gin.H{"TieuDe": "Mở Cửa Hàng Mới"})
 }
