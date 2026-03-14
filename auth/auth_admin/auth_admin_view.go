e619098092fbea670adc5f1eda19daea3f50b633
hostduong <hostduong@gmail.com>
2026-03-14 11:52:39 +0700

    Update auth_admin_view.go

--- auth/auth_admin/auth_admin_view.go
+++ auth/auth_admin/auth_admin_view.go
@@ -27,16 +27,16 @@ func checkLogin(c *gin.Context) bool {
 func TrangDangNhap(c *gin.Context) {
 	if checkLogin(c) { c.Redirect(http.StatusFound, "https://shop.99k.vn/tong-quan"); return }
 	// Render file /themes/template_admin/dang_nhap_admin.html
-	c.HTML(http.StatusOK, "dang_nhap_admin", gin.H{"TieuDe": "Đăng Nhập Quản Trị Hệ Thống"})
+	c.HTML(http.StatusOK, "dang_nhap_admin", gin.H{"TieuDe": "Đăng Nhập Hệ Thống"})
 }
 
 func TrangDangKy(c *gin.Context) {
 	if checkLogin(c) { c.Redirect(http.StatusFound, "https://shop.99k.vn/tong-quan"); return }
 	// Render file /themes/template_admin/dang_ky_admin.html
-	c.HTML(http.StatusOK, "dang_ky_admin", gin.H{"TieuDe": "Mở Cửa Hàng Mới"})
+	c.HTML(http.StatusOK, "dang_ky_admin", gin.H{"TieuDe": "Đăng Ký Tài Khoản"})
 }
 
 func TrangQuenMatKhau(c *gin.Context) {
 	// Render file /themes/template_admin/quen_mat_khau_admin.html
-	c.HTML(http.StatusOK, "quen_mat_khau_admin", gin.H{"TieuDe": "Khôi phục Mật Khẩu Admin"})
+	c.HTML(http.StatusOK, "quen_mat_khau_admin", gin.H{"TieuDe": "Khôi Phục Mật Khẩu"})
 }
