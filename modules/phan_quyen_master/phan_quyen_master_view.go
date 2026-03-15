4f4a35a496f7306930413c267496568b7cd33749
hostduong <hostduong@gmail.com>
2026-03-15 12:53:39 +0700

    Update phan_quyen_master_view.go

--- modules/phan_quyen_master/phan_quyen_master_view.go
+++ modules/phan_quyen_master/phan_quyen_master_view.go
@@ -12,6 +12,12 @@ func TrangPhanQuyenMaster(c *gin.Context) {
 	masterID := c.GetString("SHOP_ID")
 	userID := c.GetString("USER_ID")
 
+	// [LUẬT THÉP 4]: CHỈ DUY NHẤT ID 001 MỚI ĐƯỢC VÀO TRANG NÀY
+	if userID != "0000000000000000001" {
+		c.Redirect(http.StatusFound, "/master/tong-quan")
+		return
+	}
+
 	core.KhoaHeThong.RLock()
 	kh := core.CacheMapKhachHang[core.TaoCompositeKey(masterID, userID)]
 	listPQ := core.CachePhanQuyen[masterID]
