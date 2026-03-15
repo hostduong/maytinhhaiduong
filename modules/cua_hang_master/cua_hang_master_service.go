2ccc3021a5e92416adf51ae7880f92f55f441b8f
hostduong <hostduong@gmail.com>
2026-03-15 11:30:54 +0700

    Update cua_hang_master_service.go

--- modules/cua_hang_master/cua_hang_master_service.go
+++ modules/cua_hang_master/cua_hang_master_service.go
@@ -42,11 +42,10 @@ func Service_LuuCuaHang(dto DTO_UpdateCuaHang) error {
 	
 	if dto.VaiTro != "" {
 		kh.VaiTroQuyenHan = dto.VaiTro
-		if dto.ChucVu != "" { 
-			kh.ChucVu = dto.ChucVu 
+		if dto.ChucVu != "" { kh.ChucVu = dto.ChucVu 
 		} else {
 			kh.ChucVu = dto.VaiTro
-			if pq, ok := core.CacheMapPhanQuyen[core.TaoCompositeKey(adminID, dto.VaiTro)]; ok {
+			if pq, ok := core.CacheMapPhanQuyen[core.TaoCompositeKey(dto.ShopID, dto.VaiTro)]; ok {
 				kh.ChucVu = pq.TenVaiTro
 			}
 		}
